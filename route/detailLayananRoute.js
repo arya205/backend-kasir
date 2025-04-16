d0be34cceb91167bd6fc18ddd191c4882692bd53
arya205 <syarifrahmanbego@gmail.com>
2025-04-16 10:24:00 +0700

    paling last

--- route/detailLayananRoute.js
+++ route/detailLayananRoute.js
@@ -2,6 +2,7 @@ import {
     getDetailLayanan,
     getFilePDF,
     createDetailLayanan,
+    updateDetailLayanan,
     deleteDetailLayanan,
     uploadImage,
     getDetailLayananById,
@@ -14,7 +15,7 @@ route.get('/detail-layanan/:id', getDetailLayanan)
 route.get('/detail-pdf/:id', getFilePDF)
 route.get('/detail/:id', getDetailLayananById)
 route.post('/detail', uploadImage, createDetailLayanan)
-route.patch('/detail/:id', uploadImage, createDetailLayanan)
+route.patch('/detail/:id', uploadImage, updateDetailLayanan)
 route.delete('/detail/:id', deleteDetailLayanan)
 
 export default route
